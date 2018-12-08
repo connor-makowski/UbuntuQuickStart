e01187789143f352651e9e53320f3e91b48ce3f2
Connor Makowski <conmak@mit.edu>
2018-12-08 13:18:12 -0500

    Fix Bug with Venv

--- ShellScripts/Python/VirtualenvQuickAccess.sh
+++ ShellScripts/Python/VirtualenvQuickAccess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Adding Quick Access for virtualenvs"
 echo '#Quick Access to venv activation' >> ~/.bash_aliases
-echo 'alias makevenv=python -m virtualenv venv' >> ~/.bash_aliases
+echo 'alias makevenv="python -m virtualenv venv"' >> ~/.bash_aliases
 echo 'alias vact="source venv/bin/activate"' >> ~/.bash_aliases
 echo "Done adding quick access for virtualenvs"
