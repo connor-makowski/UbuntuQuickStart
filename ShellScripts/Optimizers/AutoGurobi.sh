807dbf2dc1e1d4b24d7362b3569c96c2e06b0c8b
Connor Makowski <connorman528@gmail.com>
2019-01-07 10:45:35 -0500

    Update Auto Gurobi

--- ShellScripts/Optimizers/AutoGurobi.sh
+++ ShellScripts/Optimizers/AutoGurobi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo '' >> ~/.bashrc
+echo '# Setup Gurobi Environment Variables' >> ~/.bashrc
 echo 'export GUROBI_HOME="/opt/gurobi801/linux64"' >> ~/.bashrc
 echo 'export PATH="${PATH}:${GUROBI_HOME}/bin"' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"' >> ~/.bashrc
