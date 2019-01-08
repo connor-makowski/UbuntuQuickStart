cb3e5df64a043e320a69accd4da18e729fd667b7
Connor Makowski <connorman528@gmail.com>
2019-01-08 10:25:05 -0500

    Add in Git Installation to Basics.

--- ShellScripts/Helpers/InstallBasics.sh
+++ ShellScripts/Helpers/InstallBasics.sh
@@ -1,5 +1,8 @@
 echo "Installing curl"
-sudo apt-get install curl
+sudo apt-get install curl -y
 
 echo "Installing xclip"
-sudo apt-get install xclip
+sudo apt-get install xclip -y
+
+echo "Installing Git"
+sudo apt-get install git -y
