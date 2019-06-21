f00aff315cac7f46ef4b10f28230eaad3e55e2b6
Connor Makowski <connorman528@gmail.com>
2019-06-21 18:27:31 -0400

    Update Gurobi Install Instructions

--- ShellScripts/Optimizers/GurobiInstall.sh
+++ ShellScripts/Optimizers/GurobiInstall.sh
@@ -11,9 +11,15 @@ export PATH=$PATH:/opt/gurobi810/linux64/bin
 
 cd /opt
 tar xvzf gurobi8.1.0_linux64.tar.gz
+chmod -R 777 gurobi810
 
+#Exit out of root and then apply the gurobi licence
 #Apply Licence
 grbgetkey $gurobikey
+
+#Inside of a virtualenv navigate to gurobi810 and setup gurobipy
+# (venv) ... $ cd /opt/gurobi810/linux64
+# (venv) ... $ python setup.py install
 
 #Check Licence
 # gurobi.sh
